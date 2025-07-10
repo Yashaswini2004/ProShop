e0f961ba3b510dec0d4d8bd222ec50eb8fb06b69
Yashaswini2004 <yashaswinic31@gmail.com>
2025-07-10 21:51:10 +0530

    backend routers

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 // server.js
 import express from 'express';
-import products from './data/products.js';
+
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
-
+import productRoutes from "./routes/productRoute.js";
 // ✅ Load environment variables first
 dotenv.config();
 
@@ -21,15 +21,9 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+app.use('/api/products',productRoutes);
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
 
 // Server start
 app.listen(port, () => console.log(`Server running on port ${port}`));
